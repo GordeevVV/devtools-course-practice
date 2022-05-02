c46faff4d17227f7eabb7b658990046657bfa925
Viktor <victo31072001@gmail.com>
2022-05-02 18:25:39 +0300

    fix style

--- modules/pichugin_i_sort/src/sort_selection_application.cpp
+++ modules/pichugin_i_sort/src/sort_selection_application.cpp
@@ -28,14 +28,16 @@ operator()(int argc, const char** argv) {
             std::string error_msg = "Error occurred: Should be more arguments.";
             throw std::runtime_error(error_msg);
         }
-        int Arr[]={parseArgument(argv[1]), parseArgument(argv[2]), parseArgument(argv[3]),
-                   parseArgument(argv[4]), parseArgument(argv[5]), parseArgument(argv[6]),
+        int Arr[]={parseArgument(argv[1]), parseArgument(argv[2]),
+                   parseArgument(argv[3]),
+                   parseArgument(argv[4]), parseArgument(argv[5]),
+                   parseArgument(argv[6]),
                    parseArgument(argv[7]), parseArgument(argv[8])};
         SortSelection res(Arr, 8);
 
         res.Sort();
 
-        result=res.Ex(Arr, 8);
+        result = res.Ex(Arr, 8);
     }
     catch (std::exception& exp) {
         return exp.what();
